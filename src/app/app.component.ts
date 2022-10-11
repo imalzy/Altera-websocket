91799d315387ba8e052663b1c09b1abc07938cb4
imalzy <imalzaya19@gmail.com>
2022-10-11 19:45:39 +0700

    update websocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { Component, OnInit } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { concatMap, delay } from 'rxjs/operators';
+import { fromEvent, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ws-bitcoin';
+  data: any;
+  bitcoin:number[] = [];
+  websocket$: WebSocketSubject<any> | undefined;
+
+  ngOnInit(): void {
+
+  }
+
+  startWs(): void {
+    this.websocket$ = webSocket(environment.websocket_bitcoint);
+
+    this.websocket$
+      .pipe(concatMap((item) => of(item).pipe(delay(1000))))
+      .subscribe((res: any) => {
+        console.log(res.bitcoin)
+        this.bitcoin.push(res.bitcoin);
+
+        this.data = {
+          labels: [1, 2, 3],
+          datasets: [
+            {
+              label: 'Bitcoin',
+              data: [this.bitcoin],
+              borderColor: '#FFA726',
+              tension: 0.4,
+            },
+          ],
+        };
+      });
+
+    this.websocket$ = webSocket({
+      url: environment.websocket_bitcoint,
+      serializer: val => JSON.stringify({channel: '', val})
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  websocket_bitcoint: 'wss://ws.coincap.io/prices?assets=bitcoin',
 };
 
 /*
